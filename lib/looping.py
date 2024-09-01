e9299a92eda3e5e387cd8c6392c83e34d757f1ef
joshua mwendwa <leeroyjoshu087@gmail.com>
2024-09-01 17:58:52 +0300

    joshua

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,30 @@
+
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
+    i= 10
+    while i>-1:
+        if(i>0):
+            print(i)
+        else:
+            print("Happy New Year!")
+        i-=1
+
     pass
 
 def square_integers(int_list):
-    # code goes here!
+    int_list = [num*num for num in int_list]
+    return int_list
     pass
 
 def fizzbuzz():
-    # code goes here!
+    for i in range(1,101):
+        if(i%5 == 0 and i%3 == 0):
+           print("FizzBuzz")  
+        elif(i%3 == 0):
+            print("Fizz")
+        elif(i%5 == 0):
+            print("Buzz")
+        else:
+            print(i)
     pass
